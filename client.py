f12fd8206b8d9c6fb80d1e12d00edf19b06c5a63
Isa <nfleishman@student.ccaeagles.org>
2023-08-25 12:07:25 -0400

    Encryption update

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ from cryptography.fernet import Fernet
 
 host = "127.0.0.1"
 checkinport = 1024 #default "checkin" port
-encryption = "" #set to whatever encryption key
+encryption = False #An ecryption key will be generated in the handshake? [True/False]
 presharedencryption = "" #set to whatever PRESHARED encryption key
 authcode = "" #set if you have set password authentication on the host
 refrences = "" #will set a text value in the client connection table
