bad88de78f4588d8d21ad996f81a5088c8404e63
Isa <nfleishman@student.ccaeagles.org>
2023-08-25 16:48:24 +0000

    connection shenanigans

--- host.py
+++ host.py
@@ -5,12 +5,31 @@ from cryptography.fernet import Fernet
 
 host = "127.0.0.1"
 checkinport = 1024 #default "checkin" port
-presharedencryption = "" #set to whatever PRESHARED encryption key
+presharedencryption = b'ts1UOpWUIE-PFmHE-HWhVWKS5YPrfSQzHjwFm7wnCIQ=' #set to whatever PRESHARED encryption key
 authcode = "" #add the password if you want to enable password authentication
+portrange = [1025, 65536] #Excluding default checkin port lowest port at 0 value highest at 1
+portblacklist = [] #any ports you dont want being used
 
 def waitconnect(host, checkinport, cuemax):
-    initsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    initsocket.bind((host, checkinport))
-    initsocket.listen(cuemax)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as initsocket:
+        initsocket.bind((host, checkinport))
+        initsocket.listen(cuemax)
+        clientsocket, address = initsocket.accept()
+        with clientsocket:
+            print("Connected @", address)
+            while True:
+                try:
+                    try:
+                        data = json.loads(clientsocket.recv(checkinport).decode("utf-8"))
+                    except:
+                        fernet = Fernet(presharedencryption)
+                        data2 = clientsocket.recv(checkinport)
+                        print(data2)
+                        data = json.loads(fernet.decrypt(data2).decode("utf-8"))
+                        break
+                    print(data)
+                except:
+                    pass
+
 
 waitconnect(host, checkinport, 5)
--- target.py
+++ target.py
@@ -6,6 +6,19 @@ from cryptography.fernet import Fernet
 host = "127.0.0.1"
 checkinport = 1024 #default "checkin" port
 encryption = False #An ecryption key will be generated in the handshake? [True/False]
-presharedencryption = "" #set to whatever PRESHARED encryption key
+presharedencryption = b'ts1UOpWUIE-PFmHE-HWhVWKS5YPrfSQzHjwFm7wnCIQ=' #set to whatever PRESHARED encryption key
 authcode = "" #set if you have set password authentication on the host
-refrences = "" #will set a text value in the client connection table
+refrence = "" #will set a text value in the client connection table
+
+def connnect():
+    if presharedencryption != "":
+        fernet = Fernet(presharedencryption)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
+        serversocket.connect((host, checkinport))
+        if presharedencryption == "":
+            serversocket.sendall(bytes(json.dumps({"refrence": refrence, "auth": authcode, "encrypt": encryption, "intergrity": True}),encoding="utf-8"))
+        else:
+            x = fernet.encrypt(bytes(json.dumps({"refrence": refrence, "auth": authcode, "encrypt": encryption, "intergrity": True}),encoding="utf-8"))
+            print(x)
+            serversocket.sendall(x)
+connnect()
